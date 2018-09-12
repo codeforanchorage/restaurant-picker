0e694e1d99911ebf17543f9d9ded29758c0f7f1a
Nigel Kibodeaux <nigelmail@gmail.com>
2018-09-11 21:01:44 -0800

    put stuff in the server dir

--- server/routes/api.js
+++ server/routes/api.js
@@ -17,6 +17,23 @@ router.get('/', function(req, res, next) {
 
   request.get(options, (error, response, body) => {
     if (error) return next(error)
+    res.set('Access-Control-Allow-Origin', 'http://localhost:4200')
+    res.send(body)
+  })
+})
+
+router.get('/business', function(req, res, next) {
+  let options = {
+    method: 'GET',
+    url: 'https://api.yelp.com/v3/businesses/' + req.query.id,
+    headers: {
+      Authorization: 'Bearer ' + process.env.TOKEN
+    }
+  }
+
+  request.get(options, (error, response, body) => {
+    if (error) return next(error)
+    res.set('Access-Control-Allow-Origin', 'http://localhost:4200')
     res.send(body)
   })
 })
